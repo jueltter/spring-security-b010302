85d5cac4b0f65214a9be3e20a36e9862a0efb279
Stalin Amagua <jueltter@users.noreply.github.com>
2026-02-18 23:08:43 -0500

    initial commit

---
+++ src/main/java/tech/samagua/spring_security_b010302/SpringSecurityB010302Application.java
@@ -0,0 +1,13 @@
+package tech.samagua.spring_security_b010302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityB010302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityB010302Application.class, args);
+	}
+
+}
